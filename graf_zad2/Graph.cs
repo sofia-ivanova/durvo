f32332887d4c3c9865ae66146ebd5d4a3fa4e468
Sofia Ivanova <sofia.iivanova@gmail.com>
2025-05-05 13:21:05 +0300

    ,

---
+++ graf_zad2/Graph.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graf_zad2
+{
+    internal class Graph
+    {
+        private int vertices;
+        private List<int>[] adjList;
+        public Graph(int v)
+        {
+            vertices = v;
+            adjList = new List<int>[v];
+
+            for (int i = 0; i < v; i++)
+            {
+                adjList[i] = new List<int>();
+            }
+        }
+        public void AddEdge(int u, int v)
+        {
+            adjList[u].Add(v);
+            adjList[v].Add(u);
+        }
+        public int FindShortestPath(int start, int target)
+        {
+            bool[] visited = new bool[vertices];
+            int[] distance = new int[vertices];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(start);
+            visited[start] = true;
+            distance[start] = 0;
+            while (queue.Count > 0)
+            {
+                int current = queue.Dequeue();
+                foreach (int neighbor in adjList[current])
+                {
+                    if (!visited[neighbor])
+                    {
+                        visited[neighbor] = true;
+                        distance[neighbor] = distance[current] + 1;
+                        queue.Enqueue(neighbor);
+                        if (neighbor == target)
+                            return distance[neighbor];
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ graf_zad2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graf_zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfVertices = 5;
+            Graph g = new Graph(numberOfVertices);
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 3);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 4);
+            int start = 0;
+            int target = 4;
+            int steps = g.FindShortestPath(start, target);
+            if (steps != -1)
+                Console.WriteLine($"Най-краткият път от {start} до {target} е {steps} стъпки.");
+            else
+                Console.WriteLine("Няма път между върховете.");
+            Console.ReadKey();
+        }
+    }
+}
