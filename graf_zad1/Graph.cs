e646d12d62139da41bbef513df049d01177d919f
Sofia Ivanova <sofia.iivanova@gmail.com>
2025-05-05 13:17:48 +0300

    ,

---
+++ graf_zad1/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graf_zad1
+{
+    internal class Graph
+    {
+        private int vertices; // брой върхове
+        private List<int>[] adjList;
+        public Graph(int v)
+        {
+            vertices = v;
+            adjList = new List<int>[vertices];
+            for (int i = 0; i < vertices; i++)
+            {
+                adjList[i] = new List<int>();
+            }
+        }
+        public void AddEdge(int u, int v)
+        {
+            adjList[u].Add(v);
+            adjList[v].Add(u);
+        }
+        public void RemoveEdge(int u, int v)
+        {
+            adjList[u].Remove(v);
+            adjList[v].Remove(u); 
+        }
+        public void PrintGraph()
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                Console.Write($"Връх {i}: ");
+                foreach (int neighbor in adjList[i])
+                {
+                    Console.Write(neighbor + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ graf_zad1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graf_zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(5);
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 4);
+            g.AddEdge(1, 2);
+            g.AddEdge(1, 3);
+            g.AddEdge(1, 4);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 4);
+            Console.WriteLine("Граф преди премахване на ребро 1-4:");
+            g.PrintGraph();
+            g.RemoveEdge(1, 4);
+            Console.WriteLine("Граф след премахване на ребро 1-4:");
+            g.PrintGraph();
+            Console.ReadKey();
+        }
+    }
+}
