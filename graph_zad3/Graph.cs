382d59ded618bcdcd78264ca2db67a159c06beca
Sofia Ivanova <sofia.iivanova@gmail.com>
2025-05-05 13:23:48 +0300

    .

---
+++ graph_zad3/Graph.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_zad3
+{
+    internal class Graph
+    {
+        private int vertices;
+        private List<int>[] adjList;
+        public Graph(int n)
+        {
+            vertices = n;
+            adjList = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adjList[i] = new List<int>();
+            }
+        }
+        public void AddEdge(int u, int v)
+        {
+            adjList[u].Add(v);
+            adjList[v].Add(u);
+        }
+        public bool DFS(int start, int end)
+        {
+            bool[] visited = new bool[vertices];
+            return DFSUtil(start, end, visited);
+        }
+        private bool DFSUtil(int current, int target, bool[] visited)
+        {
+            if (current == target)
+                return true;
+
+            visited[current] = true;
+
+            foreach (int neighbor in adjList[current])
+            {
+                if (!visited[neighbor])
+                {
+                    if (DFSUtil(neighbor, target, visited))
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ graph_zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Graph g = new Graph(n);
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(2, 3);
+            int start = 0;
+            int end = 3;
+            bool pathExists = g.DFS(start, end);
+            Console.WriteLine(pathExists ? "Да" : "Не");
+            Console.ReadKey();
+        }
+    }
+}
